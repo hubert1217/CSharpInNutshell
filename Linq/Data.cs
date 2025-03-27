02f28b30b3d85d998263246f8a47ca37cc3dd711
hubert1217 <hubert.roj@gmail.com>
2025-03-27 19:55:01 +0100

    update Linq

---
+++ Linq/Data.cs
@@ -0,0 +1,38 @@
+﻿using Linq.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public static class Data
+    {
+        public static List<Person> GetEmployeeList() => new List<Person>()
+        {
+            new Person { Id = 1, FirstName = "Jan", LastName = "Kowalski", Age = 30, Country = "Polska" },
+            new Person { Id = 2, FirstName = "Anna", LastName = "Nowak", Age = 25, Country = "Polska" },
+            new Person { Id = 3, FirstName = "Marek", LastName = "Wiśniewski", Age = 40, Country = "Polska" },
+            new Person { Id = 4, FirstName = "Piotr", LastName = "Wójcik", Age = 35, Country = "Niemcy" },
+            new Person { Id = 5, FirstName = "Katarzyna", LastName = "Kamińska", Age = 28, Country = "Polska" },
+            new Person { Id = 6, FirstName = "Tomasz", LastName = "Zieliński", Age = 45, Country = "Polska" },
+            new Person { Id = 7, FirstName = "Agnieszka", LastName = "Dąbrowska", Age = 32, Country = "Polska" },
+            new Person { Id = 8, FirstName = "Grzegorz", LastName = "Szymański", Age = 38, Country = "Polska" },
+            new Person { Id = 9, FirstName = "Magdalena", LastName = "Lewandowska", Age = 29, Country = "Niemcy" },
+            new Person { Id = 10, FirstName = "Robert", LastName = "Woźniak", Age = 50, Country = "Polska" },
+            new Person { Id = 11, FirstName = "Paweł", LastName = "Kaczmarek", Age = 26, Country = "Polska" },
+            new Person { Id = 12, FirstName = "Barbara", LastName = "Jankowska", Age = 34, Country = "Polska" },
+            new Person { Id = 13, FirstName = "Marcin", LastName = "Mazur", Age = 37, Country = "Niemcy" },
+            new Person { Id = 14, FirstName = "Ewa", LastName = "Krawczyk", Age = 42, Country = "Polska" },
+            new Person { Id = 15, FirstName = "Krzysztof", LastName = "Piotrowski", Age = 31, Country = "Polska" },
+            new Person { Id = 16, FirstName = "Natalia", LastName = "Grabowska", Age = 27, Country = "Polska" },
+            new Person { Id = 17, FirstName = "Andrzej", LastName = "Nowicki", Age = 36, Country = "Hiszpania" },
+            new Person { Id = 18, FirstName = "Joanna", LastName = "Adamczyk", Age = 33, Country = "Rosja" },
+            new Person { Id = 19, FirstName = "Michał", LastName = "Dudek", Age = 44, Country = "Polska" },
+            new Person { Id = 20, FirstName = "Sylwia", LastName = "Pawlak", Age = 39, Country = "Hiszpania" }
+        };
+    }
+}
+
+
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Linq;
+
+Console.WriteLine("Linq");
+
+
+var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+var result = numbers.Where(x=>x%2==0).ToList();
+
+
+var employeeList = Data.GetEmployeeList();
+
+var ageBetween40and45 = (from employee in employeeList
+                         where employee.Age >= 40 && employee.Age <= 45
+                         select employee).ToList();
+
+
+var _ageBetween40and45 = employeeList.Where(x => x.Age >= 40 && x.Age <= 45).ToList();
+
+
+var fromSpain = employeeList.Where(x => x.Country.Contains("Hiszpania")).ToList();
+
+
+
+var groupByCountry = employeeList.GroupBy(x => x.Country).Select(x => new { Country = x.Key, employee=x.ToList() }).ToList();
+
+
+
+
+
+
+Console.WriteLine(string.Join(", ", result));
+
+Console.ReadLine();
